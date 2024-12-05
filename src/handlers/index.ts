2b428cb217fb230e559022c9108d9c2f8fb65aff
germanp007 <pintogerman281@gmail.com>
2024-12-05 10:49:33 -0300

    Continuacion curso React-Node

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,13 +1,33 @@
 import User from "../models/User";
+import { Request, Response } from "express";
+import slug from "slug";
+import { hashpassword } from "../utils/auth";
 
-export const createAccount = async (req, res) => {
+export const createAccount = async (req: Request, res: Response) => {
   try {
-    await User.create(req.body);
-    res.json({ message: "Respuesta enviada" });
+    const { email, password } = req.body;
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      const error = new Error("El usuario Ya esta registrado");
+      res.status(409).json({ error: error.message });
+      return;
+    }
+    const handle = slug(req.body.handle);
+    const handleExist = await User.findOne({ handle });
+    if (handleExist) {
+      const error = new Error("El usuario Ya esta registrado");
+      res.status(409).json({ error: error.message });
+      return;
+    }
 
-    /*otra forma de hacerlo es instanciar user
-      const user = new User(req.body)
-       user.save() */
+    const user = new User(req.body);
+    // Para enciptar el password
+    user.password = await hashpassword(password);
+    user.handle = handle; // Tiene un segundo parametro para separar las palabras
+    // puede ser - _ '' etc...
+    await user.save();
+
+    res.status(201).json({ message: "Respuesta enviada" });
   } catch (error) {
     res.status(500).send("Error al crear usuario");
   }
